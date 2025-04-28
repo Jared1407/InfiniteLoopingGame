59601242f729dbec7077f9153c90b07bde598924
Jared <jdguthri@asu.edu>
2025-04-28 12:37:38 -0700

    Room script should be close to done depending, floor in progress

--- new-game-project/scripts/floor.gd
+++ new-game-project/scripts/floor.gd
@@ -1,10 +1,54 @@
 extends Node2D
+class_name Floor
 
+# Exports
+@export var room_scene: PackedScene
+
+# Constants
+const MAX_ROOMS := 3
+
+# Vars
+var rooms: Array[Room] = []
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
+	populate_rooms()
 	pass # Replace with function body.
 
+func populate_rooms():
+	for i in range(MAX_ROOMS):
+		add_room(i)
+
+func add_room(slot_index: int) -> void:
+	# If at max capacity do nothing: NEED: add error message to user
+	if rooms.size() >= MAX_ROOMS:
+		return
+		
+	# Create a variable that represents a new room scene
+	var room = room_scene.instantiate() as Room
+	room.position = Vector2(slot_index * room.get_width(), 0)
+	# Put the rooms under the 'rooms' node
+	$Rooms.add_child(room)
+	# Add these rooms to the array created earlier
+	rooms.append(room)
+	
+	
+func remove_room(slot_index: int) -> void:
+	if slot_index < 0 or slot_index >= rooms.size():
+		return
+	
+	var room = rooms[slot_index]
+	rooms.remove_at(slot_index)
+	room.queue_free()
+	# Need to fill the space, probably need a dummy room to go in the space
+	reposition_rooms()
+
+func reposition_rooms() -> void:
+	for i in range(rooms.size()):
+		rooms[i].position = Vector2(i * rooms[i].get_width(),0)
+		
+func setup_portals() -> void:
+	pass
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
--- new-game-project/scripts/room.gd
+++ new-game-project/scripts/room.gd
@@ -1,10 +1,18 @@
 extends Node2D
-
+class_name Room
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
+	# Init room type and visuals
+	# Enrique !!
 	pass # Replace with function body.
 
+# The art/texture will decide h/w
+func get_width() -> float:
+	return get_node("RoomArt").texture.get_width()
+
+func get_height() -> float:
+	return get_node("RoomArt").texture.get_height()
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
